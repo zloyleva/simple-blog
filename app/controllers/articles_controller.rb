5ff3b4c890469554554b6c7d7082984796799666
zloyleva <zloyleva@gmail.com>
2016-05-29 23:45:09 +0300

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,17 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: :new 
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)    
   end
 
 end
